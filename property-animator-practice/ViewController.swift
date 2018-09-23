62c434c8fce789919dc8f454ec52a4cf3005c050
Shima <shima.jin@icloud.com>
2018-09-24 00:53:03 +0900

    Initial Commit

---
+++ property-animator-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  property-animator-practice
+//
+//  Created by Jinsei Shima on 2018/09/24.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
